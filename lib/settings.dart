fdfa6d45390b0785acc1356d602154f14e0c7f09
anasassad <lwalida1921@gmail.com>
2024-11-20 00:11:35 +0100

    Using the GridView.Count() constructor in the Stateful Settings Widget

--- lib/settings.dart
+++ lib/settings.dart
@@ -13,3 +13,25 @@ class SettingsScreen extends StatelessWidget {
     );
   }
 }
+
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      childAspectRatio: 3,
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      scrollDirection: Axis.vertical,
+      padding: const EdgeInsets.all(20),
+      children: const <Widget>[],
+    );
+  }
+}
